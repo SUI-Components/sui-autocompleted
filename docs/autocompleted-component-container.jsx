206ff89fe331003faf978a260bbb4f4c957a66c2
Miguel Ángel Durán <miguel@miduga.es>
2017-10-30 11:42:14 +0100

    Move to prop-types and react 15

--- docs/autocompleted-component-container.jsx
+++ docs/autocompleted-component-container.jsx
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {Autocompleted} from '../src'
 import suggests from './mock_suggests'
 
