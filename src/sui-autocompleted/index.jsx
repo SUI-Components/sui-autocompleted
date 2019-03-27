52878d10e049433c6272058abdd3ba90ee14b329
Juan Diego Mendez <juandiego.mendez@schibsted.com>
2019-03-27 11:58:46 +0100

    fix lint

--- src/sui-autocompleted/index.jsx
+++ src/sui-autocompleted/index.jsx
@@ -1,191 +1,198 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import ResultsList from "./results-list";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ResultsList from './results-list'
 
-const DELTA_MOVE = 1;
-const DOWN = "ArrowDown";
-const ENTER = "Enter";
-const ESCAPE = "Escape";
-const UP = "ArrowUp";
+const DELTA_MOVE = 1
+const DOWN = 'ArrowDown'
+const ENTER = 'Enter'
+const ESCAPE = 'Escape'
+const UP = 'ArrowUp'
 
 export default class Autocompleted extends Component {
   constructor(...args) {
-    super(...args);
+    super(...args)
 
-    const { selectFirstByDefault, initialValue, focus } = this.props;
+    const { selectFirstByDefault, initialValue, focus } = this.props
 
-    this.input = null;
-    this.wrapper = null;
-    this.defaultPosition = selectFirstByDefault ? 0 : -1;
+    this.input = null
+    this.wrapper = null
+    this.defaultPosition = selectFirstByDefault ? 0 : -1
 
     this.state = {
       active: this.defaultPosition,
       focus: focus,
       showResultList: false,
       value: initialValue
-    };
+    }
   }
 
   moveDown = () => {
-    const { active } = this.state;
-    const lastPosition = this.props.suggests.length - 1;
-    return active === lastPosition ? active : active + DELTA_MOVE;
-  };
+    const { active } = this.state
+    const lastPosition = this.props.suggests.length - 1
+    return active === lastPosition
+      ? active
+      : active + DELTA_MOVE
+  }
 
   moveUp = () => {
-    const { active } = this.state;
-    return active === this.defaultPosition ? active : active - DELTA_MOVE;
-  };
+    const { active } = this.state
+    return active === this.defaultPosition
+      ? active
+      : active - DELTA_MOVE
+  }
 
   componentDidMount() {
     if (this.state.focus) {
-      this.focusInput();
+      this.focusInput()
     }
 
-    document.addEventListener("click", this.documentClickHandler, false);
+    document.addEventListener('click', this.documentClickHandler, false)
   }
 
   componentWillUnmount() {
-    document.removeEventListener("click", this.documentClickHandler, false);
+    document.removeEventListener('click', this.documentClickHandler, false)
   }
 
   componentWillReceiveProps({ focus }) {
     if (this.state.focus !== focus) {
-      this.setState({ focus });
+      this.setState({ focus })
     }
   }
 
   componentWillUpdate(nextProps, { focus }) {
     if (focus) {
-      this.focusInput();
+      this.focusInput()
     }
   }
 
-  upDownHandler = event => {
+  upDownHandler = (event) => {
     // Never go to negative values or value higher than the list length
-    const active = event.key === DOWN ? this.moveDown() : this.moveUp();
-    this.setState({ active });
-    event.stopPropagation();
-    event.preventDefault();
-  };
+    const active = event.key === DOWN
+      ? this.moveDown()
+      : this.moveUp()
+    this.setState({ active })
+    event.stopPropagation()
+    event.preventDefault()
+  }
 
   enterHandler = () => {
-    const suggest = this.props.suggests[this.state.active];
+    const suggest = this.props.suggests[this.state.active]
 
     if (suggest) {
-      const value = suggest.literal || suggest.content;
-      this.setState({ value });
-      this.handleSelect(suggest);
+      const value = suggest.literal || suggest.content
+      this.setState({ value })
+      this.handleSelect(suggest)
     }
-  };
+  }
 
   closeList() {
     this.setState({
       showResultList: false,
       active: null
-    });
+    })
   }
 
   documentClickHandler = ({ target }) => {
-    const { wrapper } = this;
-    const isClickOutside = wrapper && !wrapper.contains(target);
+    const { wrapper } = this
+    const isClickOutside = wrapper && !wrapper.contains(target)
 
-    isClickOutside && this.closeList();
-  };
+    isClickOutside && this.closeList()
+  }
 
   escapeHandler = () => {
-    this.closeList();
-  };
+    this.closeList()
+  }
 
   focusInput = () => {
-    this.input.focus();
-  };
+    this.input.focus()
+  }
 
-  handleChange = event => {
-    const value = event.target.value;
+  handleChange = (event) => {
+    const value = event.target.value
     this.setState({
       value,
       active: this.defaultPosition
-    });
-    this.props.handleChange(value);
-  };
+    })
+    this.props.handleChange(value)
+  }
 
   handleClear = () => {
     this.handleChange({
       target: {
-        value: ""
+        value: ''
       }
-    });
-    this.focusInput();
-  };
+    })
+    this.focusInput()
+  }
 
-  handleSelect = suggest => {
+  handleSelect = (suggest) => {
     this.setState({
       value: suggest.literal || suggest.content
-    });
-    this.props.handleSelect(suggest);
-  };
+    })
+    this.props.handleSelect(suggest)
+  }
 
-  handleKeyDown = event => {
+  handleKeyDown = (event) => {
     this.setState({
       showResultList: true
-    });
+    })
 
     switch (event.key) {
       case UP:
       case DOWN:
-        this.upDownHandler(event);
-        break;
+        this.upDownHandler(event)
+        break
       case ENTER:
-        this.enterHandler();
-        break;
+        this.enterHandler()
+        break
       case ESCAPE:
-        this.escapeHandler();
-        break;
+        this.escapeHandler()
+        break
     }
-  };
+  }
 
   renderResultList() {
-    const { suggests } = this.props;
-    const { active } = this.state;
+    const { suggests } = this.props
+    const { active } = this.state
 
-    return suggests && suggests.length > 0 ? (
-      <ResultsList
-        {...this.props}
-        handleSelect={this.handleSelect}
-        active={active}
-      />
-    ) : null;
+    return suggests && suggests.length > 0
+      ? (
+        <ResultsList
+          {...this.props}
+          handleSelect={this.handleSelect}
+          active={active}
+        />
+      )
+      : null
   }
 
   render() {
-    const { placeholder, handleFocus, handleBlur, title } = this.props;
-    const { value, showResultList } = this.state;
+    const { placeholder, handleFocus, handleBlur, title } = this.props
+    const { value, showResultList } = this.state
     return (
       <div
-        className="sui-Autocompleted"
-        ref={node => {
-          this.wrapper = node;
-        }}
+        className='sui-Autocompleted'
+        ref={node => { this.wrapper = node }}
       >
         <input
-          className="sui-Autocompleted-input"
+          className='sui-Autocompleted-input'
           onBlur={handleBlur}
           onChange={this.handleChange}
           onFocus={handleFocus}
           onKeyDown={this.handleKeyDown}
           placeholder={placeholder}
-          ref={node => {
-            this.input = node;
-          }}
+          ref={node => { this.input = node }}
           title={title}
-          type="text"
+          type='text'
           value={value}
         />
-        <span className="sui-Autocompleted-clear" onClick={this.handleClear} />
+        <span
+          className='sui-Autocompleted-clear'
+          onClick={this.handleClear}
+        />
         {showResultList && this.renderResultList()}
       </div>
-    );
+    )
   }
 }
 
@@ -201,11 +208,11 @@ Autocompleted.propTypes = {
   selectFirstByDefault: PropTypes.bool,
   suggests: PropTypes.array.isRequired,
   title: PropTypes.string
-};
+}
 
 Autocompleted.defaultProps = {
-  initialValue: "",
+  initialValue: '',
   selectFirstByDefault: true,
   focus: false,
   title: undefined
-};
+}
