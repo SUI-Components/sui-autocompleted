c4f043713bf842473f1773a4847c0eb2ddb23be5
midudev <miduga@gmail.com>
2019-10-23 14:12:36 +0200

    Release new version

--- src/list-item.js
+++ src/list-item.js
@@ -1,22 +1,25 @@
+/* eslint-disable react/jsx-no-bind */
 import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
 const noop = () => {}
 
-export default function ListItem ({content, handleSelect = noop, isActive, isSection, item}) {
+export default function ListItem({
+  content,
+  handleSelect = noop,
+  isActive,
+  isSection,
+  item
+}) {
   const classes = cx({
     'sui-Autocompleted-item': !isSection,
     'sui-Autocompleted-section': isSection,
     'sui-Autocompleted-item--active': isActive
   })
 
-  /* eslint react/jsx-no-bind: "warn" */
   return (
-    <li
-      className={classes}
-      onClick={handleSelect.bind(null, item)}
-    >
+    <li className={classes} onClick={handleSelect.bind(null, item)}>
       {content}
     </li>
   )
